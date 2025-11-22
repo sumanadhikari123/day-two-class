1e6f19a6b0682979cc54aa0a98e1db7cb58c8162
Suman Adhikari <133343325+sumanadhikari123@users.noreply.github.com>
2025-11-23 09:27:37 +1300

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,21 @@
+name = "Suman" # String
+age = 28 # Integer
+skills = ["phython", "SQL", "Power BI"]  # List
+education = ("BSc computer science, 2020")  # Tuple
+
+# Dictionary
+contact_details = {
+    "email": "suman@gmail.com",
+    "phone" : "123456",
+}
+
+# Set
+certifications = {"Azure", "AWS", "Azure"}  
+
+
+print(name)
+print(age)
+print(skills)
+print(education)
+print(contact_details)
+print(certifications)
